bb1f4d1c182a40f109b9c8e9392f0c5a1f376750
bournejj <82320254+bournejj@users.noreply.github.com>
2021-08-15 14:21:02 +0100

    Add files via upload

---
+++ Joij.sql
@@ -0,0 +1,12 @@
+Join the two tables so that every column and record appears, regardless of if there is not an owner_id.
+
+- SELECT * FROM owners FULL JOIN vehicles ON vehicles.owner_id = owners.id;
+
+	•	Count the number of cars for each owner. Display the owners first_name, last_name and count of vehicles. The first_name should be ordered in ascending order. Your output should look like this:
+
+- SELECT first_name, last_name, count(*) AS count FROM owners JOIN vehicles ON vehicles.owner_id = owners.id GROUP BY first_name, last_name ORDER BY count;
+
+	•	Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name, last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. Your output should look like this:
+
+
+SELECT first_name, last_name,AVG (ROUND(price)) AS average_price, count(*) AS count FROM owners JOIN vehicles ON vehicles.owner_id = owners.id GROUP BY first_name, last_name HAVING count(*) > 1 AND AVG(price) > 10000 ORDER BY first_name desc;
